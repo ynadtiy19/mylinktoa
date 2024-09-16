fa22a5fa6896237ef28ed2274f306ff0103db704
maludeuu <limoree28@gmail.com>
2024-09-16 15:59:38 +0800

    ok

--- routes/linka.dart
+++ routes/linka.dart
@@ -1,6 +1,20 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  // TODO: implement route handler
-  return Response(body: 'This is a new route!');
+  //medium/s?q=apple%20iphone%2013
+  final request = context.request;
+
+  // Access the query parameters as a `Map<String, String>`.
+  final params = request.uri.queryParameters;
+
+  // Get the value for the key `name`.
+  // Default to `there` if there is no query parameter.
+  var query = params['q'] ?? 'how are you buddy??';
+
+  final String apiUrl =
+      'https://mydiumtify.globeapp.dev/chattext?q=${Uri.encodeComponent(query)}';
+  print(apiUrl);
+
+  final response = await http.get(Uri.parse(apiUrl));
+  return Response.json(body: response);
 }
